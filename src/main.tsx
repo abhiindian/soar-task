75d14d8a83af18480eae2f4eb8618c30f911314d
Abhishek <abhi.chakravarti2000@gmail.com>
2025-02-18 03:18:00 +0530

    Implemented React Router

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,18 @@ import { CssBaseline, ThemeProvider } from '@mui/material'
 import theme from './theme.tsx'
 import { Provider } from 'react-redux'
 import store from './state/store.ts'
+import { BrowserRouter, Route, Routes } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <App />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+          </Routes>
+        </BrowserRouter>
       </ThemeProvider>
     </Provider>
   </StrictMode>,
